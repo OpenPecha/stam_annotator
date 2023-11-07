9fe8b1118521d2d7c2899c2b3e24c7666bcce9e6
Tenzin <tenzintsunduebhattu@gmail.com>
2023-11-07 09:52:27 +0530

    UPDATE_yml_test_file_with_P000218

--- tests/test_load_yaml_annotations.py
+++ tests/test_load_yaml_annotations.py
@@ -7,25 +7,21 @@ def test_load_annotations_from_yaml():
     # opf_author.yml has only one annotation
     yaml_file_path = Path(__file__).parent.absolute() / "data" / "opf_author.yml"
     yaml_annotations = load_annotations_from_yaml(yaml_file_path)
-    assert yaml_annotations["id"] == "73d9280f0b9448ed981250b232e0f411"
+    assert yaml_annotations["id"] == "5a54033501934d03bf5b8543542d9d6d"
     assert yaml_annotations["annotation_type"] == "Author"
     assert yaml_annotations["revision"] == "00001"
-    assert yaml_annotations["annotations"][0]["span"]["start"] == 113
-    assert yaml_annotations["annotations"][0]["span"]["end"] == 137
+    assert yaml_annotations["annotations"][0]["span"]["start"] == 19
+    assert yaml_annotations["annotations"][0]["span"]["end"] == 83
 
     # opf_quotations.yml has more than one annotation
     yaml_file_path = Path(__file__).parent.absolute() / "data" / "opf_quotations.yml"
     yaml_annotations = load_annotations_from_yaml(yaml_file_path)
-    assert yaml_annotations["id"] == "f4ab2bb05e7d4757bf65c6d06046cf76"
+    assert yaml_annotations["id"] == "10eb4f16d43f41fe90064829cf241d18"
     assert yaml_annotations["annotation_type"] == "Quotation"
     assert yaml_annotations["revision"] == "00001"
-    assert yaml_annotations["annotations"]["21f9e2dafb3a48dc9d7c34e22fa60eb9"] == {
-        "span": {"start": 45402, "end": 45527}
+    assert yaml_annotations["annotations"]["a1f3e26bdb08449aaece0bba4ac4f5cc"] == {
+        "span": {"start": 16302, "end": 16302}
     }
-    assert yaml_annotations["annotations"]["1466fe93cebb4c69b38c09dec2fad984"] == {
-        "span": {"start": 96462, "end": 96587}
+    assert yaml_annotations["annotations"]["670003b927a54a5aad937c6ae0206e61"] == {
+        "span": {"start": 16444, "end": 16444}
     }
-
-
-if __name__ == "__main__":
-    test_load_annotations_from_yaml()
