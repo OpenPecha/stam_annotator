7d4f83001193ab0e746e856aec64613c4754e813
Tenzin <tenzintsunduebhattu@gmail.com>
2023-11-24 16:10:14 +0530

    UPDATE_function_get_annotations_with_filter_method

--- src/stam_annotator/load_stam.py
+++ src/stam_annotator/load_stam.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import Union
 
 import stam
-from stam import AnnotationData, AnnotationDataSet, Annotations, AnnotationStore
+from stam import AnnotationDataSet, Annotations, AnnotationStore
 
 from stam_annotator.config import OPF_DIR
 
@@ -21,24 +21,17 @@ def get_annotation_data_set(store: AnnotationStore, key: str) -> AnnotationDataS
     return None
 
 
-def get_annotation_data(store: AnnotationStore, key: str, value: str) -> AnnotationData:
-    data_set = get_annotation_data_set(store, key)
-    for annotation_data in data_set.__iter__():
-        if annotation_data.value().__str__() == value:
-            return annotation_data
-    return None
-
-
 def get_annotations(store: AnnotationStore, key: str, value: str) -> Annotations:
-    annotation_data = get_annotation_data(store, key, value)
-    return annotation_data.annotations()
+    data_set = get_annotation_data_set(store, key)
+    data_key = data_set.key(key)
+    return data_set.data(filter=data_key, value=value).annotations()
 
 
 if __name__ == "__main__":
-    file_path = OPF_DIR / "Author.json"
+    file_path = OPF_DIR / "Sabche.json"
     store = load_stam_from_json(file_path)
-    annotations = get_annotations(store, "Structure Type", "Author")
-    for annotation in annotations:
+    annotations = get_annotations(store, "Structure Type", "Sabche")
+    for annotation in store.annotations():
         # get the text to which this annotation refers (if any)
         try:
             text = str(annotation)
