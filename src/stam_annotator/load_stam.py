30581cc5ed916971f692b0dcca669df39189e22e
Tenzin <tenzintsunduebhattu@gmail.com>
2023-11-24 12:38:15 +0530

    FEAT_get_annotations

--- src/stam_annotator/load_stam.py
+++ src/stam_annotator/load_stam.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import Union
 
 import stam
-from stam import AnnotationData, AnnotationDataSet, AnnotationStore
+from stam import AnnotationData, AnnotationDataSet, Annotations, AnnotationStore
 
 from stam_annotator.config import OPF_DIR
 
@@ -29,11 +29,22 @@ def get_annotation_data(store: AnnotationStore, key: str, value: str) -> Annotat
     return None
 
 
+def get_annotations(store: AnnotationStore, key: str, value: str) -> Annotations:
+    annotation_data = get_annotation_data(store, key, value)
+    return annotation_data.annotations()
+
+
 if __name__ == "__main__":
     file_path = OPF_DIR / "Author.json"
     store = load_stam_from_json(file_path)
-    data_set = get_annotation_data_set(store, "Structure Type")
-    annotation_data = get_annotation_data(store, "Structure Type", "Author")
-    print(annotation_data)
-    print(type(annotation_data))
-    print(annotation_data.id())
+    annotations = get_annotations(store, "Structure Type", "Author")
+    for annotation in annotations:
+        # get the text to which this annotation refers (if any)
+        try:
+            text = str(annotation)
+        except stam.StamError:
+            text = "n/a"
+        for data in annotation:
+            print(
+                "\t".join((annotation.id(), data.key().id(), str(data.value()), text))
+            )
