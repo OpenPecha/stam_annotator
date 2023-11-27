2f87b3a84550ace1bf20249e29d089a6b725191d
Tenzin <tenzintsunduebhattu@gmail.com>
2023-11-27 16:51:29 +0530

    UPDATE_resource_attribute_with_annotation_in_Annotation_Store

--- src/stam_annotator/annotation_store.py
+++ src/stam_annotator/annotation_store.py
@@ -1,4 +1,5 @@
-from typing import List
+from pathlib import Path
+from typing import List, Union
 
 from pydantic import BaseModel, Field, field_validator
 
@@ -10,7 +11,7 @@ from stam_annotator.opf_annotations_loader import (
     Span,
     create_opf_annotation_instance,
 )
-from stam_annotator.utility import get_uuid
+from stam_annotator.utility import get_filename_without_extension, get_uuid
 
 
 class Annotation_Data(BaseModel):
@@ -21,10 +22,16 @@ class Annotation_Data(BaseModel):
     store_id: str
 
 
+class Resource(BaseModel):
+    resource_id: str
+    text: str
+
+
 class Annotation(BaseModel):
     annotation_id: str
     span: Span
     annotation_data: Annotation_Data
+    resource_id: str
 
 
 class Annotation_Data_Set(BaseModel):
@@ -52,11 +59,6 @@ class Annotation_Data_Set(BaseModel):
             self.annotation_datas.append(annotation_data)
 
 
-class Resource(BaseModel):
-    resource_id: str
-    text: str
-
-
 class Annotation_Store(BaseModel):
     store_id: str
     datasets: List[Annotation_Data_Set] = Field(default_factory=list)
@@ -68,6 +70,9 @@ class Annotation_Store(BaseModel):
     def set_id(cls, v):
         return v or get_uuid()
 
+    def add_resource(self, resource: Resource):
+        self.resources.append(resource)
+
     def add_data_set(self, data_set: Annotation_Data_Set):
         self.datasets.append(data_set)
 
@@ -75,13 +80,24 @@ class Annotation_Store(BaseModel):
         self.annotations.append(annotation)
 
 
+def create_resource(file_path: Union[str, Path]):
+    resource_id = get_filename_without_extension(file_path)
+    text = Path(file_path).read_text(encoding="utf-8")
+    return Resource(resource_id=resource_id, text=text)
+
+
 def create_data_set(data_set_id: str, data_set_key: KeyEnum):
     return Annotation_Data_Set(data_set_id=data_set_id, data_set_key=data_set_key)
 
 
-def create_annotation(annotation_id: str, span: Span, annotation_data: Annotation_Data):
+def create_annotation(
+    annotation_id: str, span: Span, annotation_data: Annotation_Data, resource_id: str
+):
     return Annotation(
-        annotation_id=annotation_id, span=span, annotation_data=annotation_data
+        annotation_id=annotation_id,
+        span=span,
+        annotation_data=annotation_data,
+        resource_id=resource_id,
     )
 
 
@@ -93,10 +109,15 @@ def add_annotation_data_to_data_set(
 
 
 def opf_annotation_to_annotation_store_format(
-    opf_annot: OpfAnnotation, data_set_key: KeyEnum
+    opf_annot: OpfAnnotation,
+    data_set_key: KeyEnum,
+    resource_file_path: Union[str, Path],
 ) -> Annotation_Store:
     store_id = get_uuid()
     annotation_store = Annotation_Store(store_id=store_id)
+    resource = create_resource(resource_file_path)
+    annotation_store.add_resource(resource)
+
     data_set = create_data_set(data_set_id=opf_annot.id, data_set_key=data_set_key)
     annotation_store.add_data_set(data_set)
 
@@ -109,7 +130,10 @@ def opf_annotation_to_annotation_store_format(
     for id, annotation in opf_annot.annotations.items():
         add_annotation_data_to_data_set(data_set, annotation_data)
         annotation = create_annotation(
-            annotation_id=id, span=annotation.span, annotation_data=annotation_data
+            annotation_id=id,
+            span=annotation.span,
+            annotation_data=annotation_data,
+            resource_id=resource.resource_id,
         )
         annotation_store.add_annotation(annotation)
 
@@ -118,9 +142,12 @@ def opf_annotation_to_annotation_store_format(
 
 if __name__ == "__main__":
     segment_yaml_path = OPF_DIR / "Sabche.yml"
+    resource_file_path = OPF_DIR / "v001.txt"
     opf_data_dict = load_opf_annotations_from_yaml(segment_yaml_path)
 
     opf_obj = create_opf_annotation_instance(opf_data_dict)
     data_set_key = KeyEnum.structure_type
-    stam_model = opf_annotation_to_annotation_store_format(opf_obj, data_set_key)
+    stam_model = opf_annotation_to_annotation_store_format(
+        opf_obj, data_set_key, resource_file_path
+    )
     print(stam_model)
--- src/stam_annotator/opf_annotation_to_stam.py
+++ src/stam_annotator/opf_annotation_to_stam.py
@@ -1,6 +1,3 @@
-from pathlib import Path
-from typing import Union
-
 from stam import AnnotationStore, Offset, Selector
 
 from stam_annotator.annotation_store import (
@@ -11,16 +8,14 @@ from stam_annotator.config import OPF_DIR
 from stam_annotator.enums import KeyEnum
 from stam_annotator.load_yaml_annotations import load_opf_annotations_from_yaml
 from stam_annotator.opf_annotations_loader import create_opf_annotation_instance
-from stam_annotator.utility import get_filename_without_extension, save_annotation_store
+from stam_annotator.utility import save_annotation_store
 
 
 def create_annotationstore(id: str):
     return AnnotationStore(id=id)
 
 
-def create_resource(store: AnnotationStore, text_file_path: Path):
-    text = text_file_path.read_text(encoding="utf-8")
-    resource_id = get_filename_without_extension(text_file_path)
+def create_resource(store: AnnotationStore, resource_id: str, text: str):
     return store.add_resource(id=resource_id, text=text)
 
 
@@ -34,13 +29,15 @@ def create_annotation(store: AnnotationStore, id: str, target: Selector, data: d
     return store.annotate(id=id, target=target, data=data)
 
 
-def opf_annotation_store_to_stam(
-    annotation_store: Annotation_Store, text_file_path: Union[str, Path]
-):
+def opf_annotation_store_to_stam(annotation_store: Annotation_Store):
     # Create annotation store
     store = create_annotationstore(id=annotation_store.store_id)
     # Create resource
-    resource = create_resource(store=store, text_file_path=Path(text_file_path))
+
+    for resource in annotation_store.resources:
+        create_resource(
+            store=store, resource_id=resource.resource_id, text=resource.text
+        )
     # Create dataset
     data_set = annotation_store.datasets[0]
     dataset = create_dataset(
@@ -58,7 +55,8 @@ def opf_annotation_store_to_stam(
             store=store,
             id=annotation.annotation_id,
             target=Selector.textselector(
-                resource, Offset.simple(annotation.span.start, annotation.span.end)
+                store.resource(id=annotation.resource_id),
+                Offset.simple(annotation.span.start, annotation.span.end),
             ),
             data=data,
         )
@@ -67,17 +65,17 @@ def opf_annotation_store_to_stam(
 
 if __name__ == "__main__":
     # Define your file paths and other parameters
-    text_file_path = OPF_DIR / "v001.txt"
+    resource_file_path = OPF_DIR / "v001.txt"
     annotation_yaml_path = OPF_DIR / "Author.yml"
 
     opf_data_dict = load_opf_annotations_from_yaml(annotation_yaml_path)
 
     opf_obj = create_opf_annotation_instance(opf_data_dict)
     data_set_key = KeyEnum.structure_type
-    annotation_store = opf_annotation_to_annotation_store_format(opf_obj, data_set_key)
-    stam_store_object = opf_annotation_store_to_stam(
-        annotation_store=annotation_store, text_file_path=text_file_path
+    annotation_store = opf_annotation_to_annotation_store_format(
+        opf_obj, data_set_key, resource_file_path
     )
+    stam_store_object = opf_annotation_store_to_stam(annotation_store=annotation_store)
 
     output_file_path = OPF_DIR / "stam.json"
     save_annotation_store(stam_store_object, output_file_path)
