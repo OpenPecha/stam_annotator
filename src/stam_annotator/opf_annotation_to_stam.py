8b4dd6e2e7aa572a85cf85cd682e6f44e5e0bd69
Tenzin <tenzintsunduebhattu@gmail.com>
2023-11-27 15:49:53 +0530

    UPDATE_opf_annotation_stam_using_annotation_store_class_instance

--- src/stam_annotator/opf_annotation_to_stam.py
+++ src/stam_annotator/opf_annotation_to_stam.py
@@ -3,14 +3,15 @@ from typing import Union
 
 from stam import AnnotationStore, Offset, Selector
 
+from stam_annotator.annotation_store import (
+    Annotation_Store,
+    opf_annotation_to_annotation_store_format,
+)
 from stam_annotator.config import OPF_DIR
+from stam_annotator.load_stam import KeyEnum
 from stam_annotator.load_yaml_annotations import load_opf_annotations_from_yaml
 from stam_annotator.opf_annotations_loader import create_opf_annotation_instance
-from stam_annotator.utility import (
-    get_filename_without_extension,
-    get_uuid,
-    save_annotation_store,
-)
+from stam_annotator.utility import get_filename_without_extension, save_annotation_store
 
 
 def create_annotationstore(id: str):
@@ -23,9 +24,9 @@ def create_resource(store: AnnotationStore, text_file_path: Path):
     return store.add_resource(id=resource_id, text=text)
 
 
-def create_dataset(store: AnnotationStore, id: str, key: str):
+def create_dataset(store: AnnotationStore, id: str, key: KeyEnum):
     dataset = store.add_dataset(id=id)
-    dataset.add_key(key)
+    dataset.add_key(key.value)
     return dataset
 
 
@@ -33,51 +34,50 @@ def create_annotation(store: AnnotationStore, id: str, target: Selector, data: d
     return store.annotate(id=id, target=target, data=data)
 
 
-def annotation_pipeline(
-    annotationstore_id: str,
-    text_file_path: Union[str, Path],
-    annotation_yaml_path: Union[str, Path],
-    dataset_key: str,
-) -> None:
+def opf_annotation_store_to_stam(
+    annotation_store: Annotation_Store, text_file_path: Union[str, Path]
+):
     # Create annotation store
-    store = create_annotationstore(id=annotationstore_id)
+    store = create_annotationstore(id=annotation_store.store_id)
     # Create resource
     resource = create_resource(store=store, text_file_path=Path(text_file_path))
     # Create dataset
-    yaml_data = load_opf_annotations_from_yaml(Path(annotation_yaml_path))
-    annotation_doc = create_opf_annotation_instance(yaml_data)
-
-    dataset = create_dataset(store=store, id=annotation_doc.id, key=dataset_key)
-
-    unique_uuid = get_uuid()
+    data_set = annotation_store.datasets[0]
+    dataset = create_dataset(
+        store=store, id=data_set.data_set_id, key=data_set.data_set_key
+    )
     # Create annotation
-    for id, value in annotation_doc.annotations.items():
-
+    for annotation in annotation_store.annotations:
+        annotation_data = annotation.annotation_data
         data = dataset.add_data(
-            dataset_key, annotation_doc.annotation_type, unique_uuid
+            annotation_data.annotation_data_key.value,
+            annotation_data.annotation_data_value.value,
+            annotation_data.annotation_data_id,
         )
         create_annotation(
             store=store,
-            id=id,
+            id=annotation.annotation_id,
             target=Selector.textselector(
-                resource, Offset.simple(value.span.start, value.span.end)
+                resource, Offset.simple(annotation.span.start, annotation.span.end)
             ),
             data=data,
         )
-
-    output_file_name = get_filename_without_extension(annotation_yaml_path)
-    output_file_path = OPF_DIR / f"{output_file_name}.json"
-    save_annotation_store(store, output_file_path)
+    return store
 
 
 if __name__ == "__main__":
     # Define your file paths and other parameters
-    annotationstore_id = "P000218_Volume_1"
     text_file_path = OPF_DIR / "v001.txt"
-    annotation_yaml_path = OPF_DIR / "Quotation.yml"
-    dataset_key = "Structure Type"
+    annotation_yaml_path = OPF_DIR / "Author.yml"
 
-    # Run the pipeline
-    annotation_pipeline(
-        annotationstore_id, text_file_path, annotation_yaml_path, dataset_key
+    opf_data_dict = load_opf_annotations_from_yaml(annotation_yaml_path)
+
+    opf_obj = create_opf_annotation_instance(opf_data_dict)
+    data_set_key = KeyEnum.structure_type
+    annotation_store = opf_annotation_to_annotation_store_format(opf_obj, data_set_key)
+    stam_store_object = opf_annotation_store_to_stam(
+        annotation_store=annotation_store, text_file_path=text_file_path
     )
+
+    output_file_path = OPF_DIR / "stam.json"
+    save_annotation_store(stam_store_object, output_file_path)
