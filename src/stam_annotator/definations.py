cd98f8f2757848853d37c8432ed9c182c152f56b
Tenzin <tenzintsunduebhattu@gmail.com>
2023-12-04 10:21:29 +0530

    UPDATE_data_opf_with_payload

--- src/stam_annotator/definations.py
+++ src/stam_annotator/definations.py
@@ -11,6 +11,8 @@ OPA_DIR = DATA_DIR / "opa"
 OPF_BO_DIR = OPF_DIR / "bo"
 OPF_EN_DIR = OPF_DIR / "en"
 
+OPF_WITH_PAYLOAD = DATA_DIR / "opf_with_payload"
+
 
 class ValueEnum(Enum):
     author = "Author"
