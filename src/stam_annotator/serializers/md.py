20c76f51c9ac21fd5f9df8f4c03573cde785d6a4
Tenzin <tenzintsunduebhattu@gmail.com>
2024-03-06 17:00:05 +0530

    serializer function

--- src/stam_annotator/serializers/md.py
+++ src/stam_annotator/serializers/md.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import Dict, List
 
+from antx import transfer
+
 from stam_annotator.config import PECHAS_PATH
 from stam_annotator.stam_fetcher.pecha import Pecha
 
@@ -8,8 +10,14 @@ from stam_annotator.stam_fetcher.pecha import Pecha
 class Pecha_MD_formatter:
     def __init__(self, pecha: Pecha):
         self.pecha = pecha
+        self.pecha_id = pecha.id_
         self.base_texts = self.load_base_texts()
         self.grouped_annotations = self.group_annotations_by_type()
+
+    @classmethod
+    def from_id(cls, pecha_id: str, github_token: str, out_path: Path = PECHAS_PATH):
+        pecha = Pecha.from_id(pecha_id, github_token, out_path)
+        return cls(pecha) if pecha else None
 
     def load_base_texts(self):
         base_texts = {}
@@ -36,10 +44,52 @@ class Pecha_MD_formatter:
                     )
         return grouped_annotations
 
-    @classmethod
-    def from_id(cls, pecha_id: str, github_token: str, out_path: Path = PECHAS_PATH):
-        pecha = Pecha.from_id(pecha_id, github_token, out_path)
-        return cls(pecha) if pecha else None
+    def serialize(self, output_dir: Path):
+        for volume_name in self.pecha.get_pecha_volume_names():
+            self.serialize_volume(volume_name, output_dir)
+
+    def serialize_volume(self, volume_name: str, output_dir: Path):
+        base_text = self.base_texts[volume_name]
+        volume_annotations = self.grouped_annotations[volume_name]
+        for annotation_type, annotations in volume_annotations.items():
+            base_text = self.apply_annotation(base_text, annotation_type, annotations)
+
+        output_md_file = output_dir / f"{self.pecha_id}_{volume_name}.md"
+        output_md_file.write_text(base_text)
+
+    @staticmethod
+    def apply_annotation(base_text: str, ann_type: str, annotations: List[Dict]) -> str:
+        if ann_type == "BookTitle":
+            ann_style = [["BookTitle start", r"<h1>"], ["BookTitle end", r"</h1>"]]
+        if ann_type == "Chapter":
+            ann_style = [["Chapter start", r"<h2>"], ["Chapter end", r"</h2>"]]
+        if ann_type == "Sabche":
+            ann_style = [["Sabche start", r"<h3>"], ["Sabche end", r"</h3>"]]
+        if ann_type == "Author":
+            ann_style = [["Author start", r"<i>—"], ["Author end", r"—</i>"]]
+        if ann_type == "Yigchung":
+            ann_style = [["Yigchung start", r"<i>"], ["Yigchung end", r"</i>"]]
+        if ann_type == "Quotation":
+            ann_style = [
+                ["Quotation start", r"<blockquote>"],
+                ["Quotation end", r"</blockquote>"],
+            ]
+
+        for annotation in annotations:
+            start, end = annotation["span"]["start"], annotation["span"]["end"]
+            base_text_with_annotation = (
+                base_text[:start]
+                + ann_style[0][1]
+                + base_text[start:end]
+                + ann_style[1][1]
+                + base_text[end:]
+            )
+            result = transfer(
+                base_text_with_annotation, ann_style, base_text, output="txt"
+            )
+            base_text = result
+
+        return base_text
 
 
 if __name__ == "__main__":
@@ -47,4 +97,6 @@ if __name__ == "__main__":
 
     pecha_id = "P000216"
     formatter = Pecha_MD_formatter.from_id(pecha_id, GITHUB_TOKEN)
-    print(formatter.grouped_annotations)
+
+    output_dir = Path(".")
+    formatter.serialize(output_dir)
